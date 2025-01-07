3230aa261ddb3612c549206d3ef97ee4e4152104
Ali Fawaz <89460174+eggman920@users.noreply.github.com>
2025-01-07 17:06:53 -0500

    Update scratch_37.py
    remove hook

--- scratch_37.py
+++ scratch_37.py
@@ -6,5 +6,5 @@ msg = 'cron test'
 
 print(msg)
 
-slack = 'https://hooks.slack.com/workflows/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'
+slack = ''
 req.post(slack, json={"msg": msg})
